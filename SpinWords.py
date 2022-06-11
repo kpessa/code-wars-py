bb63ea4b9c358da8ae872ad2ede50bb862df4bf4
Kurt Pessa <kpessa@gmail.com>
2022-06-11 11:13:32 -0400

    initial commit

---
+++ SpinWords.py
@@ -0,0 +1,16 @@
+import doctest
+
+
+def spin_words(sentence):
+    """
+    Spin words in a sentence.
+
+    >>> spin_words("Hey fellow warriors")
+    'Hey wollef sroirraw'
+
+    """
+    return ' '.join(word if len(word) < 4 else word[::-1] for word in sentence.split())
+
+
+if __name__ == '__main__':
+    doctest.testmod()
